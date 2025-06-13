d706994d245fb31414e28aeee80b1542350b6d49
Rafael Almagro <r.pinheiro.almagro@avanade.com>
2025-06-12 21:03:22 -0300

    feat: Desafio contador.

---
+++ DesafioControleFluxo/src/main/java/org/example/exception/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
